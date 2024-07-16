d6e4af3bfe381d061fb0d27ecab95c94544314ad
Abdul <abdulfaim393@gmail.com>
2024-07-16 21:22:49 +0530

    git changes push check

--- portal/src/app/videoProduction/page.tsx
+++ portal/src/app/videoProduction/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import VideoProduction from "../components/user/pages/services/videoProduction/VideoProduction";
 
 const page = () => {
+  console.log("video production page");
   return (
     <>
       <VideoProduction />
